c8358b7f21b4785917cc4e0eed884bd4564d1e97
githuv4 <sendex485@gmail.com>
2020-05-12 16:24:10 +0900

    function2, return

--- pre/3-function.js
+++ pre/3-function.js
@@ -2,5 +2,20 @@ function hello(name, name2) {
   console.log("Hi,", name);
   console.log(`Hello, ${name2}`);
 }
+// hello("Theron", 22);
 
-hello("Theron", 22);
+function sayHello(name, age) {
+  console.log("Hello" + name + "You are" + age + "years old");
+  console.log(`Hello, ${name}, youa are ${age} years old`);
+}
+
+// sayHello("ss", "23");
+// const greettings = sayHello("ss", 18);
+// console.log(greettings);
+
+function sayHi(name, age) {
+  return `Hello, ${name}, youa are ${age} years old`;
+}
+console.log(sayHi("xx", 45));
+const greettings = sayHi("pp", 55);
+console.log(greettings);
---
+++ pre/4-calc.js
@@ -0,0 +1,12 @@
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+};
+
+const result1 = calculator.plus(1, 3);
+const result2 = calculator.minus(1, 3);
+console.log(result2);
