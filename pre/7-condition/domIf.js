f478eeb0d343c010e8bd410dbf1696b2909347ac
githuv4 <sendex485@gmail.com>
2020-05-15 12:49:30 +0900

    if,DOM,addEventListener

---
+++ pre/7-condition/domIf.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+const title = document.querySelector(".title");
+
+const BASE_COLOR = "blue";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
+  //   title.addEventListener("click", handleClick);
+  title.addEventListener("mouseenter", handleClick);
+}
+
+init();
