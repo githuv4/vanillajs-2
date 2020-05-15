9c9d489ce22b5c9b696ed542c1fab07aa591d6de
githuv4 <sendex485@gmail.com>
2020-05-15 13:19:55 +0900

    classList - contains

--- pre/7-condition 2/domIf.js
+++ pre/7-condition 2/domIf.js
@@ -3,9 +3,8 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentClass = title.className;
-  // console.log(currentClass);
-  if (currentClass !== CLICKED_CLASS) {
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (!hasClass) {
     title.classList.add(CLICKED_CLASS);
   } else {
     title.classList.remove(CLICKED_CLASS);
