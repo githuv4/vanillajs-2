d687309ccb62213b8388828fb7c64abc5c882a88
githuv4 <sendex485@gmail.com>
2020-05-23 00:32:20 +0900

    save the list on LS

--- 1/todo.js
+++ 1/todo.js
@@ -3,7 +3,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  console.log(btn);
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -16,6 +28,7 @@ function paintToDo(text) {
   //   const newId = toDos.length+1;
   const newId = new Date().getTime();
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
@@ -26,6 +39,28 @@ function paintToDo(text) {
     text: text,
     // id: toDos.length + 1,
     id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+function paintToDoFromLS(text, id) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  //   const newId = toDos.length+1;
+  // const newId = new Date().getTime();
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  // li.id = id;
+  toDoList.appendChild(li);
+  //LS에 추가
+  const toDoObj = {
+    text: text,
+    // id: toDos.length + 1,
+    id: id,
   };
   toDos.push(toDoObj);
   saveToDos();
@@ -43,7 +78,7 @@ function loadToDos() {
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(function (toDo) {
-      paintToDo(toDo.text);
+      paintToDoFromLS(toDo.text, toDo.id);
     });
   }
 }
