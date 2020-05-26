342fbf3355c8af4ccca2967ba7f7e644fd6ee08d
githuv4 <sendex485@gmail.com>
2020-05-27 02:48:10 +0900

    weather-navigator

---
+++ 1/weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCords = localStorage.getItem(COORDS);
+  if (loadedCords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
